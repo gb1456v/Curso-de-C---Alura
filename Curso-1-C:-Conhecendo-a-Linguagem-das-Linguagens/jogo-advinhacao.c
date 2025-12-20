891a35db247b6ef8343c729adde8391ac2252766
Gabriel Vieira <36107057+gb1456v@users.noreply.github.com>
2025-12-20 18:43:13 +0000

    melhora mensagens de saída e formatação no jogo de adivinhação

--- Curso-1-C:-Conhecendo-a-Linguagem-das-Linguagens/jogo-advinhacao.c
+++ Curso-1-C:-Conhecendo-a-Linguagem-das-Linguagens/jogo-advinhacao.c
@@ -14,7 +14,7 @@ int main(){
     int tentativas=0;
     int nivel=0;
     double pontos = 1000;
-    printf("Escolha o nível de dificuldade: (1) Fácil (2) Médio (3) Difícil:");
+    printf("Escolha o nível de dificuldade: (1) Fácil (2) Médio (3) Difícil\n");
     scanf("%d", &nivel);
     switch(nivel) {
         case 1:
@@ -27,21 +27,28 @@ int main(){
             tentativas = 6;
             break;
     }
-    for(int i= 1; i<= tentativas; i++) {
+    for(int i= 1; i <= tentativas; i++) {
         printf("Qual número você acha que é?\n");
         scanf("%d", &chute);
         if(chute==numero_secreto) {
-            printf("Parabéns!! Você acertou o número com %d tentativas\n");
+            printf("Parabéns!! Você acertou o número secreto!\n",tentativas);
+            break;
         } else {
             printf("\nVocê chutou o número %d\n", chute);
             printf("\nVocê errou!\n");
             if(chute>numero_secreto) {
-                printf("O numero Secreto é menor do que o número que você digitou!\n");
+                printf("O numero secreto é menor do que o número que você digitou!\n");
             } else {
-                printf("\nO numero Secreto é maior do que o número que você digitou!\n");
+                printf("\nO numero secreto é maior do que o número que você digitou!\n");
             }
             double pontos_perdidos = abs(((double)chute - numero_secreto) /2);
             pontos = pontos-pontos_perdidos;
+            if(tentativas > 1) {
+                printf("Você ainda tem %d tentativas\n", (tentativas - i));
+            } else {
+                printf("Você ainda tem %d tentativa\n", (tentativas - i));
+            }
+            
             printf("Você tem %2.lf pontos nesta rodada!\n", pontos);
         }
     }
